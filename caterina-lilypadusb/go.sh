8a9e540066cbfddb803f5d4cbe22eb67e85a2b66
vlcvi01 <vlcvi01@doma>
2016-09-11 20:40:09 +0200

    Externalization of  device USB ID

--- caterina-lilypadusb/go.sh
+++ caterina-lilypadusb/go.sh
@@ -4,8 +4,11 @@ make all
 avr-objcopy -I ihex Caterina.hex -O binary Caterina.bin 
 avr-objdump -j .sec1 -d -m avr5 Caterina.hex  > Caterina.asm
 #exit
-avrdude -v -v -patmega32u4 -cstk500v1 -b19200 -e -Ulock:w:0x3F:m -Uefuse:w:0xce:m -Uhfuse:w:0xd8:m -Ulfuse:w:0xff:m -P/dev/ttyUSB0
-avrdude -v -v -patmega32u4 -Uflash:w:Caterina.hex:i -Ulock:w:0x2F:m -cstk500v1 -b19200 -P/dev/ttyUSB0
+PROGRAMER=-cstk500v1 -b19200 -P/dev/ttyUSB0
+PROGRAMER=-cusbtiny
+
+avrdude -v -v -patmega32u4 -e -Ulock:w:0x3F:m -Uefuse:w:0xce:m -Uhfuse:w:0xd8:m -Ulfuse:w:0xff:m $PROGRAMER
+avrdude -v -v -patmega32u4 -Uflash:w:Caterina.hex:i -Ulock:w:0x2F:m $PROGRAMER
 
 #exit
 rm current_firmware.hex
